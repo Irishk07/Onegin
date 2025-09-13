b92b3edd28beced4a62af4ebd083832593001d77
irishk07 <prygunova.irina07@mail.ru>
2025-09-13 22:44:59 +0300

    rename functions

--- first.cpp
+++ first.cpp
@@ -1,43 +1,44 @@
 #include "first.h"
 
+#include "main.h"
 #include "string_functions.h"
 
 #include <stdio.h>
 
-void strswap(char onegin[][LEN_OF_LINE], int strok1, int strok2) {
-
-    char temp [LEN_OF_LINE] = {};
-    my_strcpy(temp, onegin[strok1]);
-    my_strcpy(onegin[strok1], onegin[strok2]);
-    my_strcpy(onegin[strok2], temp);
-}
-
-void buble_sort(int cnt_strok, char onegin[][LEN_OF_LINE]) {
-    for (int i = 0; i < cnt_strok - 1; ++i) {
-        for (int j = 0; j < cnt_strok - i - 1; ++j) {
-
-            if (my_strcmp(onegin[j], onegin[j + 1]) > 0) {
-                strswap(onegin, j, j + 1);
-            }
-        }
-    }
-}
-
-void read_onegin(char onegin[][LEN_OF_LINE]) {
+void f_read_onegin(char f_onegin[][LEN_OF_LINE]) {
     FILE *text = fopen("Onegin.txt", "r");
 
     int cnt_strok = 0;
-    for (; fgets(onegin[cnt_strok], LEN_OF_LINE, text); ++cnt_strok) {
+    for (; fgets(f_onegin[cnt_strok], LEN_OF_LINE, text); ++cnt_strok) {
     }
 
     fclose(text);
 }
 
-void puts_onegin(int cnt_strok, char onegin[][LEN_OF_LINE]) {
+void f_strswap(char f_onegin[][LEN_OF_LINE], int strok1, int strok2) {
+
+    char temp [LEN_OF_LINE] = {};
+    my_strcpy(temp, f_onegin[strok1]);
+    my_strcpy(f_onegin[strok1], f_onegin[strok2]);
+    my_strcpy(f_onegin[strok2], temp);
+}
+
+void f_buble_sort(int cnt_strok, char f_onegin[][LEN_OF_LINE]) {
+    for (int i = 0; i < cnt_strok - 1; ++i) {
+        for (int j = 0; j < cnt_strok - i - 1; ++j) {
+
+            if (my_strcmp(f_onegin[j], f_onegin[j + 1]) > 0) {
+                f_strswap(f_onegin, j, j + 1);
+            }
+        }
+    }
+}
+
+void f_puts_onegin(int cnt_strok, char f_onegin[][LEN_OF_LINE]) {
     FILE *text_sort = fopen("Onegin_sort.txt", "w");
 
     for (int i = 0; i < cnt_strok; ++i) {
-        fputs(onegin[i], text_sort);
+        fputs(f_onegin[i], text_sort);
     }
 
     fclose(text_sort);
--- first.h
+++ first.h
@@ -1,15 +1,14 @@
 #ifndef FIRST_H_
 #define FIRST_H_
 
-const int LEN_OF_FILE = 8500;
-const int LEN_OF_LINE = 200;
+#include "main.h"
 
-void strswap(char onegin[][LEN_OF_LINE], int strok1, int strok2);
+void f_read_onegin(char f_onegin[][LEN_OF_LINE]);
 
-void buble_sort(int cnt_strok, char onegin[][LEN_OF_LINE]);
+void f_strswap(char f_onegin[][LEN_OF_LINE], int strok1, int strok2);
 
-void read_onegin(char onegin[][LEN_OF_LINE]);
+void f_buble_sort(int cnt_strok, char f_onegin[][LEN_OF_LINE]);
 
-void puts_onegin(int cnt_strok, char onegin[][LEN_OF_LINE]);
+void f_puts_onegin(int cnt_strok, char f_onegin[][LEN_OF_LINE]);
 
 #endif //FIRST_H
