f797da0e2d84bfe0dda07f7244ab0f35f7c12f95
irishk07 <prygunova.irina07@mail.ru>
2025-09-17 22:47:35 +0300

    fix заполнения и check errors

--- input.cpp
+++ input.cpp
@@ -12,26 +12,56 @@ char *read_onegin(int *cnt_strok, int *text_size) {
     assert(text_size != NULL);
 
     FILE *text = fopen("Onegin.txt", "r");
+    if (text == NULL) {
+        perror("Error is");
+
+        return NULL;
+    }
+
     const char *text_name = "Onegin.txt";
 
-    struct stat text_info;
-    stat(text_name, &text_info);
+    struct stat text_info = {};
+    if (stat(text_name, &text_info) == -1) {
+        perror("Error is");
+        
+        return NULL;
+    }
 
     *text_size = (int)text_info.st_size;
 
     char * text_onegin = (char*)calloc((size_t)(*text_size + 1), sizeof(char));
+    if (text_onegin == NULL) {
+        perror("Error is");
+
+        free(text_onegin);
+
+        return NULL;
+    }
+
     char * temp_text_onegin = text_onegin;
 
     fread((char *)text_onegin, sizeof(char), (size_t)*text_size, text);
+    if (ferror(text) != 0) {
+        fprintf(stderr, "Error is: problem with reading file");
+
+        free(text_onegin);
+
+        return NULL;
+    }
 
     for ( ; *text_onegin != '\0'; ++text_onegin) {
         if (*text_onegin == '\n') {
-            //*t_text_onegin = '\0';
             *cnt_strok = *cnt_strok + 1;
         }
     }
 
-    fclose(text);
+    if (fclose(text) == EOF) {
+        perror("Error is");
+
+        free(text_onegin);
+
+        return NULL; 
+    }
 
     return temp_text_onegin;
 }
@@ -40,14 +70,17 @@ void fill_array_onegin(info_about_strings *onegin_strings, char * text_onegin, i
     assert(onegin_strings != NULL);
     assert(text_onegin != NULL);
 
-    onegin_strings[0].point = text_onegin++;
+    onegin_strings[0].point = text_onegin;
 
     int size_now = 0;
-    for (int i = 1; i < cnt_strok && *text_onegin != '\0'; ) {
+    for (int i = 0; i < cnt_strok && *text_onegin != '\0'; ) {
         ++size_now;
 
         if (*text_onegin == '\n') {
-            onegin_strings[i].point = (text_onegin + 1);
+            if (i < cnt_strok - 1) {
+                onegin_strings[i + 1].point = (text_onegin + 1);
+            }
+            
             onegin_strings[i].size = size_now;
 
             *text_onegin = '\0';
@@ -59,4 +92,6 @@ void fill_array_onegin(info_about_strings *onegin_strings, char * text_onegin, i
 
         text_onegin++;
     }
+
+
 }
